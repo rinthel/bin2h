b463a85370c6c7ce77c13894d5b3f0784a6e7a84
Rinthel Kwon <rinthel@gmail.com>
2019-04-08 17:37:00 +0900

    first version of bin2h converter

--- src/main.cpp
+++ src/main.cpp
@@ -29,8 +29,7 @@ int main(int argc, char** argv) {
         return -1;
     }
 
-    if (!verboseMode)
-        spdlog::set_level(verboseMode ? spdlog::level::info : spdlog::level::err);
+    spdlog::set_level(verboseMode ? spdlog::level::info : spdlog::level::err);
 
     for (size_t i = 0; i < inputFilenames.size(); i++) {
         spdlog::info("intput filename[{}]: {}", i, inputFilenames[i]);
@@ -40,8 +39,15 @@ int main(int argc, char** argv) {
     try {
         spdlog::info("open output file: {}", outputFilename);
         std::ofstream out(outputFilename);
-        out << fmt::format("#ifndef") << std::endl;
-        out << fmt::format("#endif") << std::endl;
+
+        std::string outputDirectoryPath, outputFilePath, outputName, outputExtension;
+        std::tie(outputDirectoryPath, outputFilePath) = bin2h::getDirectoryAndFilename(outputFilename);
+        std::tie(outputName, outputExtension) = bin2h::getNameAndExtension(outputFilePath);
+        std::string predefinedSymbol = bin2h::convertSymbol(outputName, bin2h::CaseNotation::upperSnakeCase) + "_H";
+
+        out << fmt::format("#ifndef {}", predefinedSymbol) << std::endl;
+        out << fmt::format("#define {}", predefinedSymbol) << std::endl;
+        out << std::endl;
 
         for (auto&& inputFilename: inputFilenames) {
             // get input binary file
@@ -58,8 +64,26 @@ int main(int argc, char** argv) {
             spdlog::info("read data of file {}: {}", inputFilename, filesize);
             in.read(reinterpret_cast<char*>(data.data()), filesize);
 
-            out << fmt::format("static const") << std::endl;
+            std::string inputDirectoryPath, inputFilePath, inputName, inputExtension;
+            std::tie(inputDirectoryPath, inputFilePath) = bin2h::getDirectoryAndFilename(inputFilename);
+            std::tie(inputName, inputExtension) = bin2h::getNameAndExtension(inputFilePath);
+
+            out << fmt::format("static const char {}[] = ", bin2h::convertSymbol(inputName, bin2h::CaseNotation::upperSnakeCase)) << '{' << std::endl;
+            for (size_t i = 0; i < data.size(); ++i) {
+                if (i % 8 == 0) {
+                    out << "    ";
+                }
+                out << fmt::format("0x{:02x}, ", data[i]);
+                if (i % 8 == 7) {
+                    out << std::endl;
+                }
+            }
+            if (data.size() % 8 < 7)
+                out << std::endl;
+            out << "};" << std::endl << std::endl;
         }
+
+        out << fmt::format("#endif // {}", predefinedSymbol) << std::endl;
     }
     catch (std::exception& e) {
         spdlog::error("failed to process: {}", e.what());
